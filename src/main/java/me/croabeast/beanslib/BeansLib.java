ab8daa934eb7f223b60881a55c41391269be283c
Kihsomray <zerotoil.network@gmail.com>
2022-01-31 00:43:20 -0800

    Added main class

---
+++ src/main/java/me/croabeast/beanslib/BeansLib.java
@@ -0,0 +1,17 @@
+package me.croabeast.beanslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BeansLib extends JavaPlugin {
+
+    private static JavaPlugin plugin;
+
+    public static JavaPlugin getPlugin() {
+        return plugin;
+    }
+
+    public static void setPlugin(final JavaPlugin plugin) {
+        BeansLib.plugin = plugin;
+    }
+
+}
